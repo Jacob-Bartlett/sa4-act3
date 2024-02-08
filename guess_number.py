a60cedf8620411d2240e01feafc1d8d67b2bb33e
Jacob Bartlett <jacobebartlett@gmail.com>
2024-02-07 22:12:17 -0600

    Added feature that tells the user to guess higher or lower

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,19 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = -999999999
+y_q = "Y"
 
-
-while guess != number:
-    guess = (input("What number am I thinking of? (Press q to quit) "))
-    if guess == "q":
+while y_q == "Y":
+    if y_q == "Q":
         print(f"Sorry! The number was {number}.")
         break
-    elif guess == number:
+    guess = int(input("What number am I thinking of? "))
+    if guess == number:
         print("Congratulations! You guessed the right number.")
+        break
     elif guess != number:
-        print("Try again!")
+        if guess > number:
+            print("Guess lower!")
+        else:
+            print("Guess higher!")
+        y_q = input("Try again? (Y) or Quit? (Q) ")
