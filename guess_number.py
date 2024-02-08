7d4aecc804cf86ec559a5650f6e62a43eea3107a
Jacob Bartlett <jacobebartlett@gmail.com>
2024-02-07 22:43:41 -0600

    Trying to fix merge conflict

--- guess_number.py
+++ guess_number.py
@@ -1,6 +1,8 @@
 number = 10
 
 print("I'm thinking of a number...")
+
+guess_cap = 10
 y_q = "Y"
 
 while y_q == "Y":
@@ -12,8 +14,13 @@ while y_q == "Y":
         print("Congratulations! You guessed the right number.")
         break
     elif guess != number:
+        guess_cap -= 1
+        print(f"You have {guess_cap} guesses left.")
         if guess > number:
             print("Guess lower!")
         else:
             print("Guess higher!")
-        y_q = input("Try again? (Y) or Quit? (Q) ")
+    elif guess_cap == 0:
+        print("You ran out of guesses!")
+        break
+    y_q = input("Try again? (Y) or Quit? (Q) ")
