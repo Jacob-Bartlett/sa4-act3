04389404e3505131564dcdf088445c7ff3dd64f7
Jacob Bartlett <jacobebartlett@gmail.com>
2024-02-07 22:17:42 -0600

    Added a feature that limits the amount of guesses the user has.

--- guess_number.py
+++ guess_number.py
@@ -3,6 +3,7 @@ number = 10
 print("I'm thinking of a number...")
 guess = -999999999
 
+guess_cap = 10
 
 while guess != number:
     guess = (input("What number am I thinking of? (Press q to quit) "))
@@ -12,4 +13,10 @@ while guess != number:
     elif guess == number:
         print("Congratulations! You guessed the right number.")
     elif guess != number:
+        guess_cap -= 1
+        if guess_cap == 0:
+            print("You ran out of guesses!")
+            break
+        else:
+            print(f"You have {guess_cap} guesses left.")
         print("Try again!")
